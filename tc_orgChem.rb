050d4d077449db4db2f8b46908333af116c9b55d
MacCallister Higgins <mac@nevada.unr.edu>
2015-06-14 19:42:33 -0700

    Added first compound translation test case

--- tc_orgChem.rb
+++ tc_orgChem.rb
@@ -3,9 +3,15 @@ require "test/unit"
  
 class TestCompound < Test::Unit::TestCase
  
-  def test_Compound
+  def test_CompoundConstructor
     assert_equal("[O=C(Oc1ccccc1C(=O)O)C]", (Compound.new("[O=C(Oc1ccccc1C(=O)O)C]","2-acetoxybenzoic acid" )).smile )
     assert_equal("2-acetoxybenzoic acid", (Compound.new("[O=C(Oc1ccccc1C(=O)O)C]","2-acetoxybenzoic acid" )).iupac )
+
+  end
+
+  def test_CompoundTranslation
+  	testCompound = Compound.new("[O=C(Oc1ccccc1C(=O)O)C]")
+    assert_equal("2-acetoxybenzoic acid", testCompound.iupac)
 
   end
  
