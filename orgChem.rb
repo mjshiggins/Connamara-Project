8d5779aab987395b9575458315ed96edc2b7b27e
MacCallister Higgins <mac@nevada.unr.edu>
2015-06-22 22:36:18 -0700

    Converted Pseudocode

--- orgChem.rb
+++ orgChem.rb
@@ -184,90 +184,263 @@ end
 ##################################################
 #### Class: Node
 #### Description: Node (carbon) for use in DAG structure
-#### Vertex: 
 ##################################################
 class Node
-  attr_accessor :node, :next, :explore, :branches, :carbonLen
+  attr_accessor :node, :next, :previous, :explore, :branches, :carbonLen, :explore2
 
-  def initialize(node)
+  def initialize(*node)
     # Node name will be used as locant numbering
-    @node = node
+    @node = nil
     # Branches is a bool that indicates whether or not the substituent has sub-branches
     @branches = false
     # CarbonLen is only populated for nodes that start a new or substituent chain, and indicates base (longest) length
     @carbonLen = -1
     # Create pointer hashes, scales on demand
-    @next = {}
-    @explore = {}
+    @previous = nil
+    @next = []
+    @explore = []
+    # Explore 2 is a stack for all child nodes, including mainline during findLength
+    @explore2 = []
   end
+
+  # Separates the next node in the main branch from the other branches, moves others to explore hash
+  # Requires all node.node values to be numbered in graph correctly before use
+  def rebuild()
+    tempArr = []
+    while (@next.length > 1)
+      temp = @next.pop
+      if (temp.node == 1)
+        @explore.push(temp)
+      else
+        tempArr.push(temp)
+      end
+    end
+    # Clean main carbon chains from branch explore array
+    while (@explore.length > 1)
+      temp = @explore.pop
+      if (temp.node == 1)
+        explore.push(temp)
+      end
+    end
+    @next.push(tempArr.pop)
+    # Move saved explore nodes back over
+    #while (@explore2.length > 0)
+      #explore.push(@explore2.pop)
+    #end
+  end
+
 end
 
 ##################################################
 #### Class: Graph
 #### Description: Directed Graph for representing the structure of compounds
-#### Vertex: 
 ##################################################
 class Graph
-  attr_accessor :head, :tail, :iterator, :branch, :iupac, :firstRun
+  attr_accessor :head, :tail, :iterator, :return, :iupac, :firstRun, :baseCount
 
     def initialize()
-        #@vertices = {}
         @head = nil
         @tail = nil
         @iterator = nil
         @return = {}
         @iupac = ""
         @firstRun = true
+        @baseCount = 0
 
     end
+
   
     # Adds vertex (carbon) to next available pointer in parameter (parent) node
-    def add_vertex(parentNode)
+    # Also creates reverse link for numbering function
+    def add_vertex(parentNode = Node.new)
       temp = Node.new
-      parentNode.next.push(temp)
+      if parentNode != nil
+        temp.previous = (parentNode)
+        parentNode.next.push(temp)
+      end
+      return temp
+    end
+
+    # Head getter function
+    def getHead()
+      return @head
+    end
+
+
+
+    # Rebuilds all nodes in graph
+    def rebuildAll
+    end
+
+    # Finds the longest chain from any given carbon molecule
+    # Returns either length count or pointer to tail, depending on parameter and purpose
+    def findLength(vertex, tailBool)
+      # Initializations (counter to one, clear the branch array)
+      counter = 1
+      maxVal = 0
+      branchArr = []
+      @iterator = vertex
+      flag = true
+      branchFlag = true
+
+      # Loop until max depth found, uses a simulated stack to store branches in order
+      while flag
+        # If no children pointers are found, 
+        if @iterator.next.length == 0
+          # Do max test/update
+          if counter > maxVal
+            # Update maxVal
+            maxVal = counter
+            # Update @tail pointer if new max
+            @tail = @iterator
+          end
+
+          # While flag
+          while branchFlag
+            returnNode = branchArr.pop
+            # If branch return array isnt empty
+            if returnNode != nil
+              # If explore isn't empty
+              @iterator = returnNode
+              counter = @iterator.node
+              exploreNode = @iterator.explore2.pop
+
+              if exploreNode != nil
+                # Push branch back onto stack
+                branchArr.push(@iterator)
+                # Set iterator to explore and save explore node
+                @iterator = exploreNode
+                counter += 1
+                @iterator.node = counter
+                # Flag 
+                branchFlag = false             
+              end
+            elsif returnNode == nil
+              # Set tail and renumber main carbon line
+              @tail = @iterator
+              baseCount = maxVal
+              if tailBool
+                return @tail
+              else
+                return maxVal
+              end
+              branchFlag = false
+            end
+          end
+          branchFlag = true
+
+        # If multiple pointers to children are found
+        elsif ( !tailBool && @iterator.next.length > 1)
+          # Push node to the branch return array
+          branchArr.push(@iterator)
+          # Check for a populated explore stack on node
+          if (@iterator.explore.length == 0)
+            # If explore stack empty, populate explore and explore2 with all branches
+            @iterator.next.each {|x| @iterator.explore2.push(x)}
+            @iterator.next.each {|x| @iterator.explore.push(x)}
+            @iterator.rebuild()
+          end
+          # Set iterator to pop explore
+          @iterator = @iterator.explore2.pop
+          counter += 1
+          @iterator.node = counter
+
+        # If multiple pointers to children are found on subsequent runs
+        elsif ( tailBool && @iterator.explore.length > 1)
+          # Push node to the branch return array
+          branchArr.push(@iterator)
+
+          # Set iterator to pop explore
+          @iterator = @iterator.explore2.pop
+          counter += 1
+          @iterator.node = counter
+
+        # If only one child, iterate and update counter, next.length = 1
+        elsif (@iterator.next.length == 1)
+          @iterator = iterator.next[0]
+          counter += 1
+          @iterator.node = counter
+
+        end
+      end
+    end
+
+    # Renumbers main carbon chain and rebuilds main/child nodes
+    # DEPRECATED
+    def number(tail)
+      tail.node = @baseCount
+      while(tail.previous != nil)
+        tail.rebuild()
+        tail.previous.node = (tail.node-1)
+        tail = tail.previous
+      end
+
+      if tail.node = 1
+        return true
+      end
+      return false
+    end
+
+    # Renumbers all branches (recursively) with correct locants
+    def renumber(head)
+      # Find length from head, assigning proper locants
+      head.carbonLen = findLength(head, false)
+
+      # Find branches, recursively call renumber() on each branch
+      while(head != nil)
+        # If a branch exists, renumber it and populate carbonLen fields
+        if (head.explore.length > 0)
+          head.explore.each {|x| renumber(x) }
+        end
+        head = head.next[0]
+      end
+    end
+
+    # Checks main carbon string to see if number reversal is necessary
+    # returns pointer to proper tail due to recursive structure
+    def reverseCheck(head, tail)
+    end
+
+    # Accepts computed tail as parameter and switches all previous/next pointers and @head/@tail members, renumbers carbons
+    # Used when main chain is determined to be backwards in rebuildGraph
+    def numberReverse(tail)
+
     end
 
     # Builds the entire graph from the SMILE string
     def buildGraph(smile)
+      branchArr = []
+      @iterator = nil
+
       # Prime loop with first carbon, assign @head
-      # Loop until end of string, uses a simulated stack to store branches in order
+      # Loop until end of string, uses a simulated stack to store branches in order and number
+      smile.split("").each do |i|
+        case i
         # If branch is found "(", push branch return pointer and continue
+          when "("
+            branchArr.push(@iterator)
         # If end of branch is found ")", return to "popped" branch return pointer
-        # If simply another carbon, just add the new vertex to the parent node
-        # If end of string
-      #
-    end
-
-    # Finds the longest chain from any given carbon molecule
-    def findLength(vertex)
-      # Initializations (counter to zero, clear the branch array)
-
-      # Loop until max depth found, uses a simulated stack to store branches in order
-        # If no children pointers are found
-          # Do max test/update
-
-          # While flag
-          # If pop return != nil
-            # If pop explore != nil
-              # Push branch back onto stack
-              # Set iterator to explore
-              # Flag
-          # Else if pop return == nil
-            # return max value
-
-        # If multiple pointers to children are found
-          # Push node to the branch return array
-          # Check for a populated explore stack on node
-            # If explore stack empty, populate with all branches
-          # Set iterator to pop explore
-
-        # If only one child, iterate and update counter
-    end
-
-    # Determines correct numbering scheme for compound, populates number values, carbonLen, and branches
-    def numberCompound(vertex)
-
-
+          when ")"
+            @iterator = branchArr.pop
+        # If simply another carbon, just add the new vertex to the parent node, increment and assign node value
+          when "C"
+            if(@head == nil)
+              @head = add_vertex()
+              @iterator = @head
+            else
+              temp = add_vertex(@iterator)
+              @iterator = temp
+            end
+        end
+      end
+      
+      # Determine correct numbering scheme for compound, populates number values, carbonLen, and branches
+      puts findLength(@head, false)
+      # Assign head, tail and rebuild all nodes
+      renumber(@head)
+      # Check for renumbering
+        # Reverse main chain numbers
+        # Rebuild nodes if renumbering was required (potentially not necessary)
     end
 
     # Recursively called in order to build IUPAC string
@@ -319,17 +492,9 @@ class Compound
   # Translate Method
   # Converts member smile to correct iupac format in respective data locations
   def translate()
-    # Initialize Carbon and Hydrogen Counts
-    @carbonCount = smile.count("C")
-
-    # Count the carbons for prefix
-    # DEPRECATED
-    #puts carbonCount
-    iupac.concat(prefixBuilder(carbonCount,false))
-
-
-    # Check for parantheses (Branched)
-
+    compoundGraph = Graph.new
+    compoundGraph.buildGraph(@smile)
+    iupac = compoundGraph.buildString(compoundGraph.getHead())
   end
 
   # Cleaner Method: Removes all "vowel" based naming issues
